b3abc41910688ed157f19e33cdeba23d4d597ec9
DK\IEKA <ieka@sitecore.net>
2018-01-11 18:15:35 +0200

    Fix behavior with logout due to ticket expiration.

--- src/Sitecore.Support.191728/Mvc/Pipelines/Request/RequestBegin/EnsureLoggedInForPreview.cs
+++ src/Sitecore.Support.191728/Mvc/Pipelines/Request/RequestBegin/EnsureLoggedInForPreview.cs
@@ -1,7 +1,9 @@
 ﻿using Sitecore.Configuration;
 using Sitecore.Pipelines;
+using Sitecore.Security.Authentication;
 using Sitecore.Shell.Web;
 using Sitecore.Sites;
+using Sitecore.Web.Authentication;
 using System;
 
 namespace Sitecore.Support.Mvc.Pipelines.Request.RequestBegin
@@ -10,8 +12,10 @@ namespace Sitecore.Support.Mvc.Pipelines.Request.RequestBegin
   {
     public void Process(PipelineArgs args)
     {
-      if (!Context.PageMode.IsNormal && !Context.IsLoggedIn)
-      {
+      if ((!Context.PageMode.IsNormal && !Context.IsLoggedIn)
+          || !TicketManager.IsCurrentTicketValid()
+          || AuthenticationManager.IsAuthenticationTicketExpired())
+            {
         using (new SiteContextSwitcher(Factory.GetSite("shell")))
         {
           // Use patched ShellPage class instead of original
